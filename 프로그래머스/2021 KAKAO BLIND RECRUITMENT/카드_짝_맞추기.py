e7ec5d4b191647cdd02107be4ecf6321ac99d3ec
Taeyeon <cau_chty94@naver.com>
2021-05-18 23:30:34 +0900

    2021 KAKAO BLIND RECRUITMENT 카드 짝 맞추기 진행중

---
+++ 프로그래머스/2021 KAKAO BLIND RECRUITMENT/카드_짝_맞추기.py
@@ -0,0 +1,13 @@
+def solution(board, r, c):
+    answer = 0
+    class Point:
+        row, col, cnt = 0, 0, 0
+        def __init__(self, r, c, t):
+            self.r = r
+            self.c = c
+            self.t = t
+
+    def permutate(src):
+         n
+
+    return answer
