573339047c64bd0c300a95ef1e8c0f055f017c03
Taeyeon <cau_chty94@naver.com>
2021-05-19 12:17:17 +0900

    2021 KAKAO BLIND RECRUITMENT 광고 삽입 / 시간초과 문제를 해결하기 위해 양 끝깞에 대해서 +1 -1 을 해주고 한번에 처리하였음

--- 프로그래머스/2021 KAKAO BLIND RECRUITMENT/광고_삽입.py
+++ 프로그래머스/2021 KAKAO BLIND RECRUITMENT/광고_삽입.py
@@ -1,20 +1,22 @@
 def convert(time):
     nums = time.split(':')
-    return int(nums[0]) * 60 * 60 + int(nums[1]) * 60 + int(nums[0])
+    return (int(nums[0]) * 3600) + (int(nums[1]) * 60) + (int(nums[2]))
 
 
 def solution(play_time, adv_time, logs):
     playSec = convert(play_time)
     advSec = convert(adv_time)
 
-    totalSec = [0 for _ in range(100 * 3600)]
-
+    totalSec = [0 for _ in range(playSec + 1)]
     for log in logs:
         start = convert(log[:8])
         end = convert(log[9:])
 
-        for i in range(start, end):
-            totalSec[i] += 1
+        totalSec[start] += 1
+        totalSec[end] -= 1
+
+    for i in range(1, len(totalSec)):
+        totalSec[i] += totalSec[i - 1]
 
     currSum = 0
     for i in range(advSec):
@@ -23,8 +25,6 @@ def solution(play_time, adv_time, logs):
     maxSum = currSum
     maxIdx = 0
     for i in range(advSec, playSec):
-        if i - advSec + 1 == 5455:
-            k = 1
         currSum = currSum + totalSec[i] - totalSec[i - advSec]
         if currSum > maxSum:
             maxSum = currSum
@@ -33,4 +33,5 @@ def solution(play_time, adv_time, logs):
     return "%02d:%02d:%02d" % (maxIdx // 3600, maxIdx // 60 % 60, maxIdx % 60)
 
 print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:25:50-00:48:29", "00:40:31-01:00:00", "01:37:44-02:02:30", "01:30:59-01:53:29"]))
+print(solution("99:59:59", "25:00:00", ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]))
 print(solution("50:00:00", "50:00:00", ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"]))
