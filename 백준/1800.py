2931384ed0bfbc4475e8b6b1a2acfb10c7d3938f
chty94 <49090167+chty94@users.noreply.github.com>
2021-05-31 23:09:39 +0900

    백준 1800 인터넷 설치 DFS로 작성했는데 시간초과났음

---
+++ 백준/1800.py
@@ -0,0 +1,34 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+N, P, K = map(int, input().split())
+graph = defaultdict(dict)
+for _ in range(P):
+    a, b, cost = map(int, input().split())
+    graph[a][b] = cost
+    graph[b][a] = cost
+
+min_ = float('inf')
+visited = [False for _ in range(N + 1)]
+visited[1] = True
+def dfs(cur_node, visited, total):
+    global min_
+
+    if cur_node == N:
+        total.sort(reverse=True)
+        if total[K] < min_:
+            min_ = total[K]
+        return
+
+    for next in graph[cur_node]:
+        if not visited[next]:
+            visited[next] = True
+            dfs(next, visited, total + [graph[cur_node][next]])
+            visited[next] = False
+
+dfs(1, visited, [])
+if min_ == float('inf'):
+    print(-1)
+else:
+    print(min_)
