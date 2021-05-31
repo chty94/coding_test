137c81b63a5c3a6b598af40f84ecbf6b8ba7b75e
chty94 <49090167+chty94@users.noreply.github.com>
2021-05-31 18:41:30 +0900

    시간이 너무 오래걸린 백준 1285 동전 뒤집기

--- 백준/1285.py
+++ 백준/1285.py
@@ -1,56 +1,68 @@
+import copy
 import sys
+from itertools import combinations
+
 N = int(sys.stdin.readline())
-list_ = [list(map(lambda x: '0' if x == 'H' else '1', sys.stdin.readline().rstrip())) for _ in range(N)]
 
 
-def column(bit, n):
-    check = 0
-    for i in range(n, N*(N - 1) + 1, N):
-        check = check | (1 << i)
-    return bit ^ check
+def total_count1(list_):
+    total = 0
+    for i in range(len(list_)):
+        temp = list_[i]
+        k = 0
+        while k != N:
+            if temp & (1 << k) >= 1:
+                total += 1
+            k += 1
+    return total
 
 
-def row(bit, n):
-    if n == 0:
-        check = (1 << N) - 1
-    else:
-        s, e = (n + 1) * N, n * N
-        check = ((1 << s) - 1) - ((1 << e) - 1)
-    return bit ^ check
+def str_to_bit(bl):
+    bit = []
+    for i in range(len(bl)):
+        bit.append(int('0b' + ''.join(bl[i]), 2))
+    return bit
 
 
-def COUNT(bit):
+def bit_reverse(number):
+    return number ^ ((1 << N) - 1)
+
+
+def count1(number):
     count = 0
-    for i in range(N * N):
-        check = (1 << i)
-        if bit & check >= 1:
+    for i in range(N):
+        if (1 << i) & number >= 1:
             count += 1
     return count
 
 
-bit = ''
-for i in range(len(list_)):
-    bit = ''.join(list_[i][::-1]) + bit
-bit = '0b' + bit
-bit = int(bit, 2)
-total = []
+bit_list = [list(map(lambda x: '0' if x == 'H' else '1', sys.stdin.readline().rstrip())) for _ in range(N)]
+bit = str_to_bit(bit_list)
+total = total_count1(bit)
+total_list = [x for x in range(0, N)]
 
-def dfs(bit, n, count):
-    if count >= 2 * N:
-        total.append(COUNT(bit))
-        return
+for count in range(1, N):
+    for list_ in combinations(total_list, count):
+        new_bit = copy.deepcopy(bit)
+        for l in list_:
+            new_bit[l] = bit_reverse(bit[l])
 
-    if n < N:
-        dfs(bit, n + 1, count + 1)
-        new_bit = row(bit, n)
-        dfs(new_bit, n + 1, count + 1)
+        new_bit2 = []
+        for j in range(N - 1, -1, -1):
+            value = 0
+            for i in range(N):
+                if new_bit[i] & (1 << j) >= 1:
+                    value += 2 ** (N - 1 - i)
+            new_bit2.append(value)
 
-    else:
-        dfs(bit, n + 1, count + 1)
-        new_bit = column(bit, n - N)
-        dfs(new_bit, n + 1, count + 1)
+        real_total = 0
+        for i in range(N):
+            real = count1(new_bit2[i])
+            if real > (N / 2):
+                real_total += (N - real)
+            else:
+                real_total += real
 
-dfs(bit, 0, 0)
-print(min(total)
+        total = min(total, real_total)
 
-
+print(total)
