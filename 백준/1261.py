43313063489b8bbee06b43b1e8f3c58db2bbcdf8
chty94 <49090167+chty94@users.noreply.github.com>
2021-06-04 00:29:07 +0900

    Update 1261.py

--- 백준/1261.py
+++ 백준/1261.py
@@ -1,30 +1,26 @@
 import sys
-from collections import deque
+import heapq
 
 
 M, N = map(int, sys.stdin.readline().split())
 board = [list(map(int, list(sys.stdin.readline().rstrip()))) for _ in range(N)]
 
-que = deque()
-que.append([0, 0, 0])
-dxy = [[0, 1], [1, 0]]
-total = float('inf')
+que = []
+heapq.heappush(que, [0, 0, 0])
+dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+distances = [[float('inf')] * M for _ in range(N)]
+distances[0][0] = 0
 
 while que:
-    x, y, wall = que.popleft()
+    x, y, dist = heapq.heappop(que)
     if x == N - 1 and y == M - 1:
-        total = min(total, wall)
-        continue
+        print(dist)
+        exit(0)
 
     for dx, dy in dxy:
         nx, ny = x + dx, y + dy
         if 0 <= nx < N and 0 <= ny < M:
-            if board[nx][ny] == 1:
-                que.append([nx, ny, wall + 1])
-            else:
-                que.append([nx, ny, wall])
-
-print(total)
-
-
-
+            new_distance = dist + board[nx][ny]
+            if new_distance < distances[nx][ny]:
+                distances[nx][ny] = new_distance
+                heapq.heappush(que, [nx, ny, new_distance])
