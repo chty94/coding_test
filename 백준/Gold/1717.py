ccedce74ba5fe03b4ff7b0063b0681ea63680a8b
chty94 <49090167+chty94@users.noreply.github.com>
2021-07-30 22:42:42 +0900

    Update 1717.py
    유니온 파인드로 답을 해결하였음

--- 백준/Gold/1717.py
+++ 백준/Gold/1717.py
@@ -1,16 +1,24 @@
 import sys
 
 N, M = map(int, input().split())
-new_set = [set([i]) for i in range(N + 1)]
+new_set = {i: i for i in range(N + 1)}
+
+
+def find(x):
+    if new_set[x] == x:
+        return x
+    return find(new_set[x])
 
 for _ in range(M):
     token, a, b = map(int, sys.stdin.readline().split())
+    pa, pb = find(a), find(b)
 
     if token == 1:
-        if b in new_set[a]:
+        if pa == pb:
             print('YES')
         else:
             print('NO')
     else:
-        new_set[a].update(new_set[b])
-        new_set[b].update(new_set[a])
+        if pa != pb:
+            new_set[pb] = pa
+            new_set[b] = pa
